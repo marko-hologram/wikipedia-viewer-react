2d43075a7a6e1f256695349d89fc621b03b8d8ab
Marko Antolić <tolich94@gmail.com>
2017-05-31 20:38:50 +0200

    focus on render, added codepen demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Wikipedia Viewer React
 
+[Codepen DEMO](https://codepen.io/marko-hologram/full/xdvzbG/)
+
 Wikipedia Viewer built for [Freecodecamp](http://freecodecamp.com) as a learning project utilizing Wikipedia API. This is the React recreation of the original project which is [HERE](https://github.com/marko-hologram/wikipedia-viewer)
 
 ## Resources used
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 class WikiSearch extends React.Component {
+  componentDidMount() {
+    this.searchInput.focus();
+  }
 
   render() {
     return (
       <div className="search-input">
         <form className="pure-form" onSubmit={this.props.onSubmit}>
           <div className="pure-u-1 pure-u-md-3-4">
-            <input type="text" id="search-input" onChange={this.props.onChange} className="pure-input pure-u-1" placeholder="Input your search term here..."></input>
+            <input type="text" id="search-input" ref={(input) => this.searchInput = input} onChange={this.props.onChange} className="pure-input pure-u-1" placeholder="Input your search term here..."></input>
           </div>
           <div className="pure-u-1 pure-u-md-1-4 button-wrapper">
             <button type="submit" id="search-button" className="pure-button pure-button-primary pure-u-1 pure-u-md-23-24">Search</button>
